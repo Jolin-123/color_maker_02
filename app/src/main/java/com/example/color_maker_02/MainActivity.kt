75f583010c5a965dd4bf28bb9dd91116c2d0ef1b
linj_happy <milklinschool@gmail.com>
2023-10-08 21:08:33 -0700

    switch done, working on seekbar - first commit

--- app/src/main/java/com/example/color_maker_02/MainActivity.kt
+++ app/src/main/java/com/example/color_maker_02/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.example.color_maker_02
 
+import android.content.res.ColorStateList
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.InputFilter
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.SeekBar
 import android.widget.Switch
+
 
 const val TAG = "MainActivity"
 
@@ -44,19 +48,182 @@ class MainActivity : AppCompatActivity() {
 
         buttonOne = findViewById(R.id.button_one)
 
+        // Input filter
+        val editTextOne = findViewById<EditText>(R.id.numBox_one)
+        val editTextTwo = findViewById<EditText>(R.id.numBox_two)
+        val editTextThree = findViewById<EditText>(R.id.numBox_three)
+
+        setDecimalFilter(editTextOne,3)
+        setDecimalFilter(editTextTwo,3)
+        setDecimalFilter(editTextThree,3)
+
+
+//        val decimalFilter = InputFilter { source, start, end, dest, dstart, dend ->
+//            val newInput = dest.toString().substring(0, dstart) +
+//                    source.toString().substring(start, end) +
+//                    dest.toString().substring(dend)
+//
+//            // Use a regex pattern to allow only numbers with up to 2 decimal places
+//            val pattern = "^\\d*\\.?\\d{0,2}\$".toRegex()
+//
+//            return@InputFilter if (newInput.isEmpty() || pattern.matches(newInput)) {
+//                null // Accept the input
+//            } else {
+//                ""  // Reject the input
+//            }
+//        }
+//        editText.filters = arrayOf(decimalFilter)
+
+        // Create an instance of the Rectangle class
+        var rectangle = findViewById<Rectangle>(R.id.customRectangle)
+
+        // Set initial color based on seekBar values
+        updateRectangleColor(rectangle)
+
+        // Control turn on and off for 3 switches
+        switchOnOff()
+
+        // Reset button set 3 seekBar values to 0
+        buttonOne.setOnClickListener(){
+
+            // Reset the SeekBar value to zero
+            seekBarOne.progress = 0
+            seekBarTwo.progress = 0
+            seekBarThree.progress = 0
+
+            // Turn off the switch
+            switchOne.isChecked = false
+            switchTwo.isChecked = false
+            switchThree.isChecked = false
+        }
+
+        //seekBar session 1
         seekBarOne.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
             override fun onProgressChanged(p0: SeekBar?, progress: Int, p2: Boolean) {
                 Log.i(TAG, "onProgressChanged $progress")
 
+                updateRectangleColor(rectangle)
+
+
                 // Normalize the progress value to the range 0 to 1
                 val normalizedProgress = progress.toDouble() / 100
+                val redValue = progress
 
                 // Format the normalized progress as a decimal with two decimal places
-                numBoxOne.setText(String.format("%.2f", normalizedProgress))
+                numBoxOne.setText(String.format("%.1f", normalizedProgress))
             }
 
             override fun onStartTrackingTouch(p0: SeekBar?) {}
             override fun onStopTrackingTouch(p0: SeekBar?) {}
         })
+        //seekBar Session 2
+        seekBarTwo.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(p0: SeekBar?, progress: Int, p2: Boolean) {
+                Log.i(TAG, "onProgressChanged $progress")
+
+                updateRectangleColor(rectangle)
+
+                // Normalize the progress value to the range 0 to 1
+                val normalizedProgress = progress.toDouble() / 100
+                val greenValue = progress
+
+                // Format the normalized progress as a decimal with two decimal places
+                numBoxTwo.setText(String.format("%.2f", normalizedProgress))
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {}
+            override fun onStopTrackingTouch(p0: SeekBar?) {}
+        })
+        //seekBar Session 3
+        seekBarThree.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(p0: SeekBar?, progress: Int, p2: Boolean) {
+                Log.i(TAG, "onProgressChanged $progress")
+
+                updateRectangleColor(rectangle)
+
+                // Normalize the progress value to the range 0 to 1
+                val normalizedProgress = progress.toDouble() / 100
+                val blueValue = progress
+
+                // Format the normalized progress as a decimal with two decimal places
+                numBoxThree.setText(String.format("%.2f", normalizedProgress))
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {}
+            override fun onStopTrackingTouch(p0: SeekBar?) {}
+        })
+
+
+
+//        // Set ARGB values for the paint object in the Rectangle
+//        rectangle.paint.setARGB(255, 100, 50, 150)
+//
+//        // Optionally, you can call invalidate to force a redraw
+//        rectangle.invalidate()
+//
+//        // Now you can use this rectangle in your layout or draw it on a canvas
+//        setContentView(rectangle)
+
+    } // End of onCreate function
+
+
+    // Method to update the color of the rectangle based on SeekBar values
+    private fun updateRectangleColor(rectangle: Rectangle) {
+        val redValue = seekBarOne.progress
+        val greenValue = seekBarTwo.progress
+        val blueValue = seekBarThree.progress
+
+        val red: Int = (redValue/100.0 * 255).toInt()
+        val green: Int = (greenValue/100.0 * 255).toInt()
+        val blue: Int = (blueValue/100.0 * 255).toInt()
+
+        return rectangle.setColor(red, green, blue)
+
+     //rectangle.paint.setARGB(255, redValue, greenValue, blueValue)
     }
+    private fun switchOnOff() {
+        // Set the initial state based on the switch state
+        seekBarOne.isEnabled = switchOne.isChecked
+        switchOne.setOnCheckedChangeListener { _, isChecked ->
+            // Update the seekBar state based on the switch state
+            seekBarOne.isEnabled = isChecked
+            val trackColorStateList = ColorStateList.valueOf(Color.RED)
+        }
+        seekBarTwo.isEnabled = switchTwo.isChecked
+        switchTwo.setOnCheckedChangeListener { _, isChecked ->
+            seekBarTwo.isEnabled = isChecked
+        }
+
+        seekBarThree.isEnabled = switchThree.isChecked
+        switchThree.setOnCheckedChangeListener { _, isChecked ->
+            seekBarThree.isEnabled = isChecked
+        }
+
+//        numBoxThree.isEnabled = switchThree.isChecked
+//        numBoxThree.setOnCheckedChangeListener  { _, isChecked ->
+//            numBoxThree.isEnabled = isChecked
+//        }
+    }  // switch function off
+
+
+    private fun setDecimalFilter(editText: EditText, decimalPlaces: Int) {
+        val decimalFilter = InputFilter { source, start, end, dest, dstart, dend ->
+            val newInput = dest.toString().substring(0, dstart) +
+                    source.toString().substring(start, end) +
+                    dest.toString().substring(dend)
+
+            // Use a regex pattern to allow only numbers with the specified decimal places
+            val pattern = "^\\d*\\.?\\d{0,$decimalPlaces}\$".toRegex()
+
+            return@InputFilter if (newInput.isEmpty() || pattern.matches(newInput)) {
+                null // Accept the input
+            } else {
+                ""  // Reject the input
+            }
+        }
+        editText.filters = arrayOf(decimalFilter)
+    }
+
 }
+
+
--- app/src/main/java/com/example/color_maker_02/Rectangle.kt
+++ app/src/main/java/com/example/color_maker_02/Rectangle.kt
@@ -7,23 +7,38 @@ import android.graphics.Canvas
 import android.util.AttributeSet
 import android.view.View
 import org.w3c.dom.Attr
-
-class Rectangle @JvmOverloads constructor(context: Context, attr: AttributeSet?=null, defStyleAttr: Int = 0
-): View(context, attr, defStyleAttr){
+class Rectangle @JvmOverloads constructor(context: Context, attr: AttributeSet? = null, defStyleAttr: Int = 0) :
+    View(context, attr, defStyleAttr) {
 
     val paint: Paint = Paint()
-    //paint.setColor(Color.BLUE)
+    val rectStroke: Paint = Paint()
 
+
+    // Initialize with a default color
     init {
-        //val paint: Paint = Paint()
+        //fill
         paint.color = Color.BLUE
+
+        // stroke
+        rectStroke.setStyle(Paint.Style.STROKE);
+        rectStroke.setColor(Color.BLACK);
+        rectStroke.setStrokeWidth(15F);
     }
 
-    // Also, you might want to override onDraw method and use 'paint' to draw something on the canvas
+    // Set the color based on RGB values
+    fun setColor(red: Int, green: Int, blue: Int) {
+        paint.setARGB(255, red, green, blue)
+        // Request a redraw
+        invalidate()
+    }
+
+
+    private val radius: Float = resources.displayMetrics.density * 30f
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
-
-        // Example: Draw a rectangle on the canvas using the paint color
-        canvas?.drawRect(0f, 0f, width.toFloat(), height.toFloat(), paint)
+        canvas?.drawRoundRect(0f, 0f, width.toFloat(), height.toFloat()*0.8f, radius, radius,paint)  // paint
+        canvas?.drawRoundRect(0f, 0f, width.toFloat(), height.toFloat()*0.8f, radius, radius,rectStroke) // stroke
     }
 }
+
+
